60e6fc210044f0b1acf84d9de05039b65f251ebc
Artemii <artemiithefrog95@icloud.com>
2024-01-20 18:08:20 +0100

    Initial Commit

---
+++ WhatToWatch/WhatToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToWatchApp.swift
+//  WhatToWatch
+//
+//  Created by artemiithefrog . on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
