03b4c07cc55b802f326b43c1288b813c28eeae25
Artemii <artemiithefrog95@icloud.com>
2024-01-22 23:40:53 +0100

    Created page scroller

--- WhatToWatch/ContentView.swift
+++ WhatToWatch/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentIndex: Int = 0
+    @GestureState private var dragOffset: CGFloat = 0
+    @State var colors: [Color] = [.red, .orange, .yellow, .green, .blue]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                ZStack {
+                    ForEach(0..<colors.count, id: \.self) { index in
+                        Color(colors[index])
+                            .frame(width: 300, height: 500)
+                            .cornerRadius(25)
+                            .opacity(currentIndex == index ? 1.0 : 0.5)
+                            .scaleEffect(currentIndex == index ? 1.2 : 0.8)
+                            .offset(x: CGFloat(index - currentIndex) * 300 + dragOffset,
+                                    y: 0)
+                        
+                    }
+                }
+                .gesture (
+                    DragGesture()
+                        .onEnded { value in
+                            let threshold: CGFloat = 50
+                            if value.translation.width > threshold {
+                                withAnimation {
+                                    currentIndex = max(0, currentIndex - 1)
+                                    print(currentIndex)
+                                }
+                            } else if value.translation.width < threshold {
+                                withAnimation {
+                                    currentIndex = min(colors.count - 1, currentIndex + 1)
+                                    print(currentIndex)
+                                }
+                            }
+                        }
+                )
+            }
+            .navigationTitle("ImageCarousel")
         }
-        .padding()
     }
 }
 
